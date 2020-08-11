13cb32a465c7beb0ceb3e20f5b2f92a0dc9e0469
Paulo Renato <paulogalindo94@hotmail.com>
2020-08-10 23:16:35 -0400

    Created splash screen

--- app/src/main/java/br/com/jera/moviejera/data/remote/ApiService.kt
+++ app/src/main/java/br/com/jera/moviejera/data/remote/ApiService.kt
@@ -2,18 +2,17 @@ package br.com.jera.moviejera.data.remote
 
 import br.com.jera.moviejera.data.entities.ApiSearchResponse
 import br.com.jera.moviejera.data.entities.TokenTemporary
-import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiService {
 
     @GET("authentication/guest_session/new")
-    suspend fun getToken(): Response<TokenTemporary>
+    suspend fun getToken(): TokenTemporary
 
     @GET("search/movie")
     suspend fun searchMovie(
-        @Query("query") text: String,
+        @Query("query") textSearch: String,
         @Query("page") page: Int
-    ): Response<ApiSearchResponse>
+    ): ApiSearchResponse
 }
