3d85f7213a83211e3c81b263b368ebdc44d057cd
Paulo Renato <paulogalindo94@hotmail.com>
2020-08-14 21:01:16 -0400

    Removed unused endpoint

--- app/src/main/java/br/com/jera/moviejera/data/remote/ApiService.kt
+++ app/src/main/java/br/com/jera/moviejera/data/remote/ApiService.kt
@@ -1,14 +1,10 @@
 package br.com.jera.moviejera.data.remote
 
 import br.com.jera.moviejera.data.entities.ApiSearchResponse
-import br.com.jera.moviejera.data.entities.TokenTemporary
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiService {
-
-    @GET("authentication/guest_session/new")
-    suspend fun getToken(): TokenTemporary
 
     @GET("search/movie")
     suspend fun searchMovie(
